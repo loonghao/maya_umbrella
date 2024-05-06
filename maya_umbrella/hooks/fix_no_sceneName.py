6b84e89c8c386aa72746e5ae103202e094c1821c
hotwinter0 <believe_op@foxmail.com>
2024-05-06 19:10:35 +0800

    feat: Add fix for the issue where the cmds file command cannot properly retrieve the Maya file path.

--- maya_umbrella/hooks/fix_no_sceneName.py
+++ maya_umbrella/hooks/fix_no_sceneName.py
@@ -7,7 +7,6 @@ def hook(logger):
     if not cmds.file(query=1, sceneName=1):
         sceneName = maya.OpenMaya.MFileIO().currentFile()
         sceneName = sceneName.replace("\\","/")
-
         if os.path.exists(sceneName):
             cmds.file(rename=sceneName)
 
