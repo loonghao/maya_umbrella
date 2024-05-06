9ff9514a53997e4ac3da2e9d336d341f623c0128
hotwinter0 <believe_op@foxmail.com>
2024-05-06 16:59:15 +0800

    添加修复cmds命令无法正常获取Maya文件的问题

---
+++ maya_umbrella/hooks/fix_no_sceneName.py
@@ -0,0 +1,13 @@
+# Import third-party modules
+import maya.cmds as cmds
+import os
+
+
+def hook(logger):
+    if not cmds.file(query=1, sceneName=1):
+        sceneName = maya.OpenMaya.MFileIO().currentFile()
+        sceneName = sceneName.replace("\\","/")
+
+        if os.path.exists(sceneName):
+            cmds.file(rename=sceneName)
+
