f1b47a817e4597db08c34968f169e5acb327a2d4
Choi-Jungwoo <jungwoo@cineuse.com>
2024-05-10 04:18:02 +0800

    feat: impl virus detector

--- crates/maya-umbrella/src/lib.rs
+++ crates/maya-umbrella/src/lib.rs
@@ -1,5 +1,7 @@
 mod loader;
 mod specimen;
+mod virus_detector;
 
 pub use loader::*;
 pub use specimen::*;
+pub use virus_detector::*;
---
+++ crates/maya-umbrella/src/virus_detector/text_virus_detector.rs
@@ -0,0 +1,31 @@
+use regex::Regex;
+
+use crate::{TextSpecimen, VirusDetector};
+
+impl VirusDetector for TextSpecimen {
+    type Signature = str;
+
+    fn detect<T: AsRef<Self::Signature>>(&self, signatures: &[T]) -> bool {
+        let signatures = signatures
+            .iter()
+            .map(|s| Regex::new(s.as_ref()).unwrap())
+            .collect::<Vec<_>>();
+
+        signatures
+            .into_iter()
+            .all(|signature| signature.is_match(self))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::TextSpecimen;
+
+    use super::*;
+
+    #[test]
+    fn should_text_virus_detector_works() {
+        let specimen = TextSpecimen::new("import vaccine;vaccine.fuck()");
+        assert_eq!(true, specimen.detect(&[r"import vaccine"]));
+    }
+}
