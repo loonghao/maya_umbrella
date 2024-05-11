8197e84a9e169aac765d728e8e9b8c3398e56ba3
loonghao <hal.long@outlook.com>
2024-05-11 14:43:01 +0800

    ci: Update CI
    Signed-off-by: loonghao <hal.long@outlook.com>

--- nox_actions/codetest.py
+++ nox_actions/codetest.py
@@ -4,13 +4,13 @@ import os
 # Import third-party modules
 import nox
 from nox_actions.utils import PACKAGE_NAME
-from nox_actions.utils import PROJECT_ROOT
+from nox_actions.utils import THIS_ROOT
 
 
 def pytest(session: nox.Session) -> None:
     session.install("pytest", "pytest_cov", "pytest_mock")
-    test_root = os.path.join(PROJECT_ROOT, "tests")
+    test_root = os.path.join(THIS_ROOT, "tests")
     session.run("pytest", f"--cov={PACKAGE_NAME}",
                 "--cov-report=xml:coverage.xml",
                 f"--rootdir={test_root}",
-                env={"PYTHONPATH": PROJECT_ROOT})
+                env={"PYTHONPATH": THIS_ROOT})
--- noxfile.py
+++ noxfile.py
@@ -17,12 +17,12 @@ if ROOT not in sys.path:
 from nox_actions import lint  # noqa: E402
 from nox_actions import release  # noqa: E402
 from nox_actions import run_maya  # noqa: E402
-from nox_actions import run_test  # noqa: E402
+from nox_actions import codetest  # noqa: E402
 
 
 nox.session(run_maya.run_maya, name="maya")
 nox.session(lint.lint, name="lint")
 nox.session(lint.lint_fix, name="lint-fix")
 nox.session(release.make_install_zip, name="make-zip")
-nox.session(run_test.pytest, name="pytest")
+nox.session(codetest.pytest, name="pytest")
 nox.session(release.vendoring)
